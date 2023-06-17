7fbf34e99373cdab86c87ff7db4313dcfa8591ea
u100247 <u100247@MM0012B7.local>
2023-06-17 11:52:34 +0200

    Practice Java

---
+++ Planetas.java
@@ -0,0 +1,76 @@
+// Crea una clase Planetas que tenga los siguientes parametros:
+// nombrePlaneta, numeroSatelites, radioEcuato, temperaturaMediaDiurna, distanciaTierra, distanciaSol
+// con sus metodos get y set
+
+public class Planetas {
+    private String nombrePlaneta;
+    private int numeroSatelites;
+    private double radioEcuato;
+    private double temperaturaMediaDiurna;
+    private double distanciaTierra;
+    private double distanciaSol;
+
+    public Planetas(String nombrePlaneta, int numeroSatelites, double radioEcuato, double temperaturaMediaDiurna, double distanciaTierra, double distanciaSol){
+        this.nombrePlaneta = nombrePlaneta;
+        this.numeroSatelites = numeroSatelites;
+        this.radioEcuato = radioEcuato;
+        this.temperaturaMediaDiurna = temperaturaMediaDiurna;
+        this.distanciaTierra = distanciaTierra;
+        this.distanciaSol = distanciaSol;
+    }
+
+    public String getNombrePlaneta(){
+        return this.nombrePlaneta;
+    }
+
+    public void setNombrePlaneta(String nombrePlaneta){
+        this.nombrePlaneta = nombrePlaneta;
+    }
+
+    public int getNumeroSatelites(){
+        return this.numeroSatelites;
+    }
+
+    public void setNumeroSatelites(int numeroSatelites){
+        this.numeroSatelites = numeroSatelites;
+    }
+
+    public double getRadioEcuato(){
+        return this.radioEcuato;
+    }
+
+    public void setRadioEcuato(double radioEcuato){
+        this.radioEcuato = radioEcuato;
+    }
+
+    public double getTemperaturaMediaDiurna(){
+        return this.temperaturaMediaDiurna;
+    }
+
+    public void setTemperaturaMediaDiurna(double temperaturaMediaDiurna){
+        this.temperaturaMediaDiurna = temperaturaMediaDiurna;
+    }
+
+    public double getDistanciaTierra(){
+        return this.distanciaTierra;
+    }
+
+    public void setDistanciaTierra(double distanciaTierra){
+        this.distanciaTierra = distanciaTierra;
+    }
+
+    public double getDistanciaSol(){
+        return this.distanciaSol;
+    }
+
+    public void setDistanciaSol(double distanciaSol){
+        this.distanciaSol = distanciaSol;
+    }
+
+    public String toString(){
+        return "Nombre del Planeta: " + this.nombrePlaneta + "\nNumero de Satelites: " + this.numeroSatelites + "\nRadio Ecuatorial: " 
+        + this.radioEcuato + "\nTemperatura Media Diurna: " + this.temperaturaMediaDiurna + "\nDistancia a la Tierra: " 
+        + this.distanciaTierra + "\nDistancia al Sol: " + this.distanciaSol;
+    }
+
+}
---
+++ SistemaSolar.java
@@ -0,0 +1,8 @@
+// Crea una clase SistemaSolar que tenga un metodo bigbang para instanciar las clases Sol y Planetas, para crear el sol y los planetas del sistema solar
+
+
+
+
+
+
+
